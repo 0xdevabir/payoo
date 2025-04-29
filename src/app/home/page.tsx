4a25da3e0c97cdc720dc591d96424e8bc279aafa
0xdevabir <abir573912@gmail.com>
2025-04-29 23:13:33 +0600

    st

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function home (){
+  return (
+    <div className="flex justify-center bg-[#1b1b1b]">
+      <div className='h-[100vh] w-[100vw] md:w-[600px] bg-[#F4F5F7]'>
+        
+
+        <div className="">
+          <p>5000</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,46 @@
-import Image from "next/image";
+'use client';
+import Link from 'next/link';
+import React from 'react'
 
-export default function Home() {
+export default function Login (){
+
+  function loginVerify(){
+      // window.location.href = '/home'
+     const mobileNumberText = document.getElementById('mobileNumber');
+     const pinNumberText = document.getElementById('pinNumber');
+     const mobileNumber = mobileNumberText.value;
+     const pinNumber = pinNumberText.value;
+
+     console.log(pinNumber);
+      if((pinNumber === '1234')&&(mobileNumber != ' ')){
+          window.location.href = '/home'
+      }
+      else{
+        alert('wrong number or password')
+      }
+
+  }
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex justify-center bg-[#1b1b1b]">
+      <div className='h-[100vh] w-[100vw] md:w-[600px] bg-[#F4F5F7]'>
+        
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex justify-center items-center h-[100vh]">
+              <div className="bg-white w-[90%] p-[5vh] rounded-3xl">
+                <p className='text-[16px] font-[500]'>Mobile Number</p>
+                <input id='mobileNumber' type="text" placeholder='Enter Your Number' className='mt-[2vh] bg-[#F4F5F7] py-[10px] pl-[10px] w-[100%] rounded-2xl'/>
+                <p className='text-[16px] font-[500] mt-[2vh]'>4 Digit Pin</p>
+                <input id='pinNumber' type="text" placeholder='Enter 4 Digit Pin' className='mt-[2vh] bg-[#F4F5F7] py-[10px] pl-[10px] w-[100%] rounded-2xl'/>
+                <br />  
+                <button className='bg-[#0874F2] text-white py-[10px] w-[100%] rounded-2xl mt-[2vh]' onClick={loginVerify}>Login</button>
+                {/* <Link href="/home" className='text-[#0874F2] text-[14px] font-[500] mt-[2vh]'>Create an account</Link> */}
+              </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
     </div>
-  );
+  )
 }
 
-
-
-// echo "# payoo" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin git@github.com:0xdevabir/payoo.git
-// git push -u origin main
